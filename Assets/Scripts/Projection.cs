27748c81e4ce2cf7190027de7a24852808b61ad7
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-05-18 05:30:34 +0200

    Got cannon package working partially.
    Most material colours gone, will try making SFX and VFX functional before i call it a day.

--- Assets/Scripts/Projection.cs
+++ Assets/Scripts/Projection.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Projection : MonoBehaviour
 {
-    private Scene _simulationScene;
-    private PhysicsScene _physicsScene;
     [SerializeField] private Transform _obstaclesParent;
     [SerializeField] private LineRenderer _line;
     [SerializeField] private int _maxPhysicsFrameIterations;
+    
+    private Scene _simulationScene;
+    private PhysicsScene _physicsScene;
+    private readonly Dictionary<Transform, Transform> _spawnedObjects = new();
 
     private void Start()
     {
@@ -28,13 +30,23 @@ public class Projection : MonoBehaviour
             var ghostObj = Instantiate(obj.gameObject, obj.position, obj.rotation);
             ghostObj.GetComponent<Renderer>().enabled = false;
             SceneManager.MoveGameObjectToScene(ghostObj, _simulationScene);
+            
+            if(!ghostObj.isStatic) _spawnedObjects.Add(obj, ghostObj.transform);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var item in _spawnedObjects)
+        {
+            item.Value.position = item.Key.position;
+            item.Value.rotation = item.Key.rotation;
         }
     }
 
     public void SimulateTrajectory(Ball ballPrefab, Vector3 pos, Vector3 velocity)
     {
         var ghostObj = Instantiate(ballPrefab, pos, Quaternion.identity);
-        //ghostObj.GetComponent<Renderer>().enabled = false;
         SceneManager.MoveGameObjectToScene(ghostObj.gameObject,  _simulationScene);
 
         ghostObj.Init(velocity, true);
