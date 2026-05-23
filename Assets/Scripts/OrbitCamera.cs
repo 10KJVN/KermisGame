98f2596946d48e8ca909d383ea0ca6f4050ef6d1
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-05-23 21:58:09 +0200

    Focus Radius implemented

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -6,12 +6,40 @@ public class OrbitCamera : MonoBehaviour
 {
     [SerializeField] private Transform focus;
     [SerializeField, Range(1f, 120f)] private float distance = 5f;
+    [SerializeField, Min(0f)] private float focusRadius = 1f;
+    
+    private Vector3 _focusPoint;
 
+    private void Awake()
+    {
+        _focusPoint = focus.position;
+    }
 
     private void LateUpdate()
     {
-        var focusPoint = focus.position;
+        UpdateFocusPoint();
         var lookDirection = transform.forward;
-        transform.localPosition = focusPoint - lookDirection * distance;
+        transform.localPosition = _focusPoint - lookDirection * distance;
     }
+
+    private void UpdateFocusPoint()
+    {
+        var targetPoint = focus.position;
+
+        if (focusRadius > 0f)
+        {
+            var distance = Vector3.Distance(targetPoint, _focusPoint);
+            
+            if (distance > focusRadius)
+            {
+                _focusPoint = Vector3.Lerp(targetPoint, _focusPoint, focusRadius / distance);
+            }
+        }
+        
+        else
+        {
+            targetPoint = focus.position;
+        }
+    }
+    
 }
